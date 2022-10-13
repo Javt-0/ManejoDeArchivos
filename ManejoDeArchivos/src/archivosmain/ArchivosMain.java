54e486fd7b139a68dc1c4e67fa89f4bf5e1c55d1
Javt-0 <jonathan96aguilar@gmail.com>
2022-10-13 14:14:24 +0200

    Maneo de archivos

--- ManejoDeArchivos/src/archivosmain/ArchivosMain.java
+++ ManejoDeArchivos/src/archivosmain/ArchivosMain.java
@@ -1,4 +1,9 @@
-
+/*
+    Nuestro manejo de archivos tendra los siguientes metodos
+        - Crear archivo
+        - Guardar Imformacion
+        - Eliminar imformacion de un archivo
+ */
 package archivosmain;
 
 import manejodearchivos.ManejoDeArchivos;
@@ -11,6 +16,11 @@ public class ArchivosMain {
     
      public static void main(String[] args) {
          String nombreArchivo = "Prueba.txt";
-         ManejoDeArchivos.crearArchivo(nombreArchivo);
+         //ManejoDeArchivos.crearArchivo(nombreArchivo);
+         //ManejoDeArchivos.escribirArchivo(nombreArchivo, "ha");
+         
+         ManejoDeArchivos.agregarArchivo(nombreArchivo, "Jesus");
+         //ManejoDeArchivos.leerArchivo(nombreArchivo);
+         ManejoDeArchivos.leerArchivoPorPalabra(nombreArchivo);
      }
 }
--- ManejoDeArchivos/src/manejodearchivos/ManejoDeArchivos.java
+++ ManejoDeArchivos/src/manejodearchivos/ManejoDeArchivos.java
@@ -6,8 +6,16 @@
  */
 package manejodearchivos;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -24,6 +32,76 @@ public class ManejoDeArchivos {
             e.printStackTrace(System.out);
         }
         
+    }
+    
+    //Este metodo agrega texto en el archivo sobreescribiendo 
+    public static void escribirArchivo(String nombre, String contenido){
+        PrintWriter salida = null;
+        File archivo= new File(nombre);
+        try {//se que el fichero existe
+            salida = new PrintWriter(archivo);
+            salida.println(contenido);
+            System.out.println("Se a creado el archivo");
+        } catch (FileNotFoundException ex){//el fichero no existe
+            ex.printStackTrace(System.out);
+        } finally{//este bloque siempre se ejecuta
+            salida.close();
+        }
+    }
+    
+    //Este metodo agrega texto en el archivo, no sobreescrito
+    //No sirve
+    public static void agregarArchivo(String nombre, String contenido){
+        PrintWriter salida = null;
+        File archivo = new File(nombre);
+        try {
+            salida = new PrintWriter(new FileWriter(nombre, true));
+            salida.println(contenido);
+        } catch (IOException ex) {
+            ex.printStackTrace(System.out);
+        }finally{
+            salida.close();
+        }
+    }
+    
+    public static void leerArchivo(String nombre){
+        //1.Declaramos el file
+        File archivo = new File(nombre);
+        BufferedReader entrada = null;
+        
+        try {
+            //2.creamos el descriptor de lectura del fichero
+            entrada = new BufferedReader (new FileReader(archivo));
+            String lectura = entrada.readLine();
+            //3.Ahora recorremos lectura hasta fin de fichero
+            while (lectura != null) {
+                System.out.println(lectura);
+                lectura = entrada.readLine();
+            }
+            entrada.close();
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace(System.out);
+        } catch (IOException ex){
+            ex.printStackTrace(System.out);
+        }
+    }
+    
+    public static void leerArchivoPorPalabra(String nombre){
+        File archivo = new File(nombre);
+        
+        try {
+            Scanner entrada = new Scanner(archivo);
+            int cont = 0;
+            
+            while(entrada.hasNext()){
+                String palabra = entrada.next();
+                System.out.println(palabra);
+                cont ++;
+            }
+            System.out.println("Numero de palabras del fichero es: " + cont);
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace(System.out);
+        }
     }
     
 }
